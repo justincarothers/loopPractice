15548bba9fea52574a04b8d4ceb61e2fa2a4ec8b
justincarothers <Guest@epicodus-47.local>
2016-06-07 09:10:47 -0700

    ran through for loops.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,65 @@
-for  (i = 0 ; i < 3 ; i++) {
+// for  (i = 0 ; i < 3 ; i++) {
+//
+// alert(i);
+//
+// };
+//
+// var languages = ['HTML','CSS','Javascript',];
+//
+// for (i = 0 ; i < languages.length; i++) {
+//
+//   alert("I love " + languages[i]);
+//
+// }
 
-alert(i);
 
-};
+// ========= COUNT TO 30 ========
+
+// for (i = 0 ; i <= 30 ; i++) {
+//
+//   console.log(i)
+//
+// }
+//
+//
+// =========== COUNT BY 5
+
+
+
+
+// for (i = 0 ; i <= 30 ; i++) {
+//
+//   console.log(i * 5);
+//
+// }
+
+
+
+// ========== OUTPUT 5, 10 15, 20, 25, 30
+
+// var b = [5, 10, 15, 20, 25, 30];
+//
+// for (i = 0 ; i < b.length ; i++) {
+//
+//   console.log(b[i]);
+//
+// }
+
+// ============== COUNT TO 50 =====
+
+// for (i = 0 ; i <= 30 ; i++) {
+//
+//   console.log(i);
+//
+// }
+
+
+// ============== COUNT BY 7 ========
+
+// for (i = 0 ; i*7 <= 50 ; i++){
+//
+//   console.log(i*7);
+//
+// }
+
+// ==================
