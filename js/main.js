c5ef2b7825333f0f8b7d6bf41500e746cdd8b4fc
justincarothers <Guest@epicodus-47.local>
2016-06-07 10:25:15 -0700

    basic word guesser app created and working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-var puzzleWord = "Believe you can and you're halfway there.";
+var puzzleWord = "ANIMAL";
+
+// var puzzleWordInArray = ['A','N','I','M','A','L'];
+// var newArray = []
+//
+// for (i = 0 ; i < puzzleWordInArray.length ; i++ ) {
+//
+//   if puzzleWordInArray[i] !=== ('A' || 'E' || 'I' || 'O' || 'U') {
+//
+//     console.log("running puzzle if");
+//       }
+//       console.log("running");
+//     }
+
 var puzzleHide = puzzleWord.replace(/[aeiou]/ig,'-');
 
 document.getElementById("wordId").innerHTML = puzzleHide;
+
+
+var vowels = ["a","e","i","o","u"];
+
+$(document).ready(function() {
+
+  $("#submit").click(function() {
+
+    if ($("#userInput").val() === puzzleWord) {
+
+      alert("CORRECT!")
+
+    }
+  });
+
+});
